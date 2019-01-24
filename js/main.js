657c96f60fd0d527e2b8fa245bfa53eea0ddc618
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-01-24 09:15:13 -0600

    Add more information link

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function bookSearch() {
 
     success: function(data) {
       for(var i = 0; i < data.items.length; i++) {
-        results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "<p>" + "Authors: " + data.items[i].volumeInfo.authors + "</p>" + "<p>" + "Publisher: " + data.items[i].volumeInfo.publisher + "</p>" + "<img src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">"
+        results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "<p>Authors: " + data.items[i].volumeInfo.authors + "</p>" + "<p>Publisher: " + data.items[i].volumeInfo.publisher + "</p>" + "<img src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "<a href=" + data.items[i].volumeInfo.infoLink + ">" + "More Information</a>" 
       }
     },
 
