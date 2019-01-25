f6424f8874320bc282c093b100103468bde87486
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-01-25 07:52:49 -0600

    Refactor AJAX logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-function bookSearch() {
-  var search = document.getElementById('search').value
-  document.getElementById('results').innerHTML = ""
+$(document).ready(function() {
+  $("#button").on("click", function() {
+    var search = $("#search").val()
+    document.getElementById('results').innerHTML = ""
 
-  $.ajax({
-    url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
-    dataType: "json",
-
-    success: function(data) {
-      for(var i = 0; i < data.items.length; i++) {
-        results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "<p>Authors: " + data.items[i].volumeInfo.authors + "</p>" + "<p>Publisher: " + data.items[i].volumeInfo.publisher + "</p>" + "<img src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "<a href=" + data.items[i].volumeInfo.infoLink + ">" + "More Information</a>" 
+    $.ajax({
+      method: "GET",
+      url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+      dataType: "json"
+    }).done(function(response) {
+      for(var i = 0; i < response.items.length; i++) {
+        results.innerHTML += "<h2>" + response.items[i].volumeInfo.title + "</h2>" + "<p>Authors: " + response.items[i].volumeInfo.authors + "</p>" + "<p>Publisher: " + response.items[i].volumeInfo.publisher + "</p>" + "<img src=" + response.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "<a href=" + response.items[i].volumeInfo.infoLink + ">" + "More Information</a>" 
       }
-    },
-
-    type: 'GET'
-  });
-}
-
-document.getElementById('button').addEventListener('click', bookSearch, false)
+    })
+  })
+})
