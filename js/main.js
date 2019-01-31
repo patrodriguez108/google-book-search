5cd55882d3654cc0bdb850ea27ada60ae2bb7679
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-01-31 17:57:13 -0600

    Get promise

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-$(document).ready(function() {
-  $("#button").on("click", function() {
-    var search = $("#search").val()
-    document.getElementById('results').innerHTML = ""
+document.getElementById('button').addEventListener('click', function() {
+  var search = document.getElementById('search').value
+  document.getElementById('results').innerHTML = ""
 
-    $.ajax({
-      method: "GET",
-      url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
-      dataType: "json"
-    }).done(function(response) {
-      for(var i = 0; i < response.items.length; i++) {
-        results.innerHTML += "<h2>" + response.items[i].volumeInfo.title + "</h2>" + "<p>Authors: " + response.items[i].volumeInfo.authors + "</p>" + "<p>Publisher: " + response.items[i].volumeInfo.publisher + "</p>" + "<img src=" + response.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "<a href=" + response.items[i].volumeInfo.infoLink + ">" + "More Information</a>" 
-      }
-    })
+  fetch("https://www.googleapis.com/books/v1/volumes?q=" + search).then(function(response) {
+    console.log(response.json())
   })
 })
