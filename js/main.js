5fa1e71933f3140552da9150fcecda82b0fe84df
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-01-31 18:15:49 -0600

    Add error catch

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 document.getElementById('button').addEventListener('click', function() {
-  var search = document.getElementById('search').value
-  var url = "https://www.googleapis.com/books/v1/volumes?q=" + search
+  let search = document.getElementById('search').value
+  let url = "https://www.googleapis.com/books/v1/volumes?q=" + search
   document.getElementById('results').innerHTML = ""
 
   fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    for(var i = 0; i < data.items.length; i++) {
+    for(let i = 0; i < data.items.length; i++) {
         results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "<p>Authors: " + data.items[i].volumeInfo.authors + "</p>" + "<p>Publisher: " + data.items[i].volumeInfo.publisher + "</p>" + "<img src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "<a href=" + data.items[i].volumeInfo.infoLink + ">" + "More Information</a>" 
     }
   })
+  .catch((error) => console.log(error))
 })
