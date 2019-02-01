d7dd6188e3132cee0bde822e298a75573765530b
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-01-31 18:05:16 -0600

    Enable search

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 document.getElementById('button').addEventListener('click', function() {
   var search = document.getElementById('search').value
+  var url = "https://www.googleapis.com/books/v1/volumes?q=" + search
   document.getElementById('results').innerHTML = ""
 
-  fetch("https://www.googleapis.com/books/v1/volumes?q=" + search).then(function(response) {
-    console.log(response.json())
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    for(var i = 0; i < data.items.length; i++) {
+        results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "<p>Authors: " + data.items[i].volumeInfo.authors + "</p>" + "<p>Publisher: " + data.items[i].volumeInfo.publisher + "</p>" + "<img src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">" + "<a href=" + data.items[i].volumeInfo.infoLink + ">" + "More Information</a>" 
+    }
   })
 })
