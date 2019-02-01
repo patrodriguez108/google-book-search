d8602e081f9968597b440bf0a5bbceb4c957d539
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-01-31 19:08:28 -0600

    Add condition for no results

--- js/main.js
+++ js/main.js
@@ -6,17 +6,22 @@ document.getElementById('button').addEventListener('click', function() {
   .then((response) => response.json())
   .then((data) => {
     let output = "<h2>Results</h2>"
-    data.items.forEach(function(result) {
-      output += `
-        <div>
-          <h3>${result.volumeInfo.title}</h3>
-          <p>Authors: ${result.volumeInfo.authors}</p>
-          <p>Publisher: ${result.volumeInfo.publisher}</p>
-          <img src=${result.volumeInfo.imageLinks.smallThumbnail}>
-          <p><a href=${result.volumeInfo.infoLink}>More Information</a></p>
-        </div>
-      `;
-    });
+    if (data.items !== undefined) {
+      data.items.forEach(function(result) {
+        output += `
+          <div>
+            <h3>${result.volumeInfo.title}</h3>
+            <p>Authors: ${result.volumeInfo.authors}</p>
+            <p>Publisher: ${result.volumeInfo.publisher}</p>
+            <img src=${result.volumeInfo.imageLinks.smallThumbnail}>
+            <p><a href=${result.volumeInfo.infoLink}>More Information</a></p>
+          </div>
+        `;
+      })
+    }
+    else {
+      output += '<h3>No Results</h3>'
+    };
     document.getElementById('results').innerHTML = output
   })
   .catch((error) => console.log(error))
